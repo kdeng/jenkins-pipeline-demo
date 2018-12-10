54e39ce72fe8069d6dc7562715486c00359c1429
Kefeng Deng <kefeng.deng@51any.com>
2018-12-10 21:47:52 +1300

    udated

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,24 @@
 def call(name="world") {
     println "hello ${name}"
+    pipeline {
+        agent none
+        stages {
+            stage('Test') {
+                steps {
+                    println 'test'
+                }
+            }
+            stage('Build') {
+                steps {
+                    println 'build'
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    println 'deploy'
+                }
+            }
+        }
+    }
+
 }
