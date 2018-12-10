e09853a59aae522a68bcfcd0ec49b92e1b51d39b
Kefeng Deng <kefeng.deng@51any.com>
2018-12-10 22:15:40 +1300

    updated

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,22 +1,41 @@
 def call(name="world") {
     println "hello ${name}"
-    pipeline {
-        agent none
-        stages {
-            stage('Test') {
-                steps {
-                    println 'test'
+    // pipeline {
+    //     agent none
+    //     stages {
+    //         stage('Test') {
+    //             steps {
+    //                 println 'test'
+    //             }
+    //         }
+    //         stage('Build') {
+    //             steps {
+    //                 println 'build'
+    //             }
+    //         }
+    //         stage('Deploy') {
+    //             steps {
+    //                 println 'deploy'
+    //             }
+    //         }
+    //     }
+    // }
+
+    try {
+        lock(resource: env.JOB_NAME, inversePrecedence: true) {
+            node {
+                stage('Test') {
+                    println "test"
                 }
-            }
-            stage('Build') {
-                steps {
+
+                stage('Build') {
                     println 'build'
                 }
-            }
-            stage('Deploy') {
-                steps {
+
+                stage('Deploy') {
                     println 'deploy'
                 }
+                    
             }
         }
     }
